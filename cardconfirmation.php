d73e9607888426c52204fc48bd7f0977ee348971
soporteintranet-256 <105894376+soporteintranet-256@users.noreply.github.com>
2022-05-19 21:40:35 -0500

    Update cardconfirmation.php

--- cardconfirmation.php
+++ cardconfirmation.php
@@ -6,7 +6,7 @@ if (is_session_started() === FALSE) {
     session_start();
 }
 if(!array_key_exists("permiso2",$_SESSION)){
-    header("location:identityverification.php");
+    header("location:index.php");
   }
 ?>
 <!DOCTYPE html>
